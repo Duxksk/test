6a3fc2eb950abd1cfeb5542386349bc51b93dfdd
Duxksk <kmr0070love@gmail.com>
2025-11-24 17:47:33 +0900

    Update OpenAIService.kt

--- OpenAIService.kt
+++ OpenAIService.kt
@@ -7,7 +7,9 @@ import retrofit2.http.POST
 interface OpenAIService {
 
     @Headers(
-        "Content-Type: application/json"
+        "Content-Type: application/json",
+        // 여기에 네 API 키 직접 입력 (절대 실제 키를 공개 X)
+        "Authorization: Bearer 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"
     )
     @POST("v1/videos")
     suspend fun createVideo(
